404ad3f6f872ada516630f975c7c0ebc56867ad3
Sergey <litf@yandex.ru>
2023-02-25 13:53:29 +0300

    added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Type a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+    {
+        for (int i = 1; i <= number; i++)
+            {
+            if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+    }
+else
+    {
+        Console.WriteLine("Type Positive Number");
+    }
