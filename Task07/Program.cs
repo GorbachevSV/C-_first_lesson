21ff0524e4e0782b4e52c09bc30e16a6bef2ee9b
Sergey <litf@yandex.ru>
2023-02-25 12:49:50 +0300

    added Task05 and Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6 782 -> 2 918 -> 8
+ 
+Console.Write("Type a three digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 0) number *= -1;
+if(number > 99 && number < 1000)
+    {
+        int result = number % 10;
+        Console.WriteLine("Result = " + result);
+    }
+else
+    {
+        Console.WriteLine("Error");
+    }
