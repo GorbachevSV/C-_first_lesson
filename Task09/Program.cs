32c1f5d6794df6b3dad2478707de23c7e97c36e1
Sergey <litf@yandex.ru>
2023-03-02 22:10:55 +0300

    Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,5 +6,5 @@ int number = new Random().Next(10, 100);
 Console.WriteLine($"Random number from the line 10-99 -> {number}");
 int firstDigit = number / 10;
 int secondDigit = number % 10;
-if(firstDigit > secondDigit) Console.WriteLine($"max digit of the number -> {firstDigit}");
+if (firstDigit > secondDigit) Console.WriteLine($"max digit of the number -> {firstDigit}");
 else Console.WriteLine($"max digit of the number -> {secondDigit}");
