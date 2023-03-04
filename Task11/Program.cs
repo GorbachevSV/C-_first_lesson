109d91cdefef98164c240349428615cb886b8093
Sergey <litf@yandex.ru>
2023-03-04 20:37:06 +0300

    Task12

---
+++ Task11/Program.cs
@@ -0,0 +1,19 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number from the line 100-999 -> {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine($"new number -> {result}");
+
+int sumDigit = SumDigit(number);
+
+int SumDigit(int num) // num = number
+{
+    int firstDigit = num / 100;
+    int thirdDigit = num % 10;
+    int sumD = firstDigit * 10 + thirdDigit;
+    return sumD;
+}
+Console.WriteLine($"new number -> {sumDigit}");
---
+++ Task12/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая будет принимать на 
+// вход два числа и выводить, является ли первое 
+// число кратным второму. Если число 1 не кратно 
+// числу 2, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+//16, 4 -> кратно
+
+//Console.WriteLine("Type first number");
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Type second number");
+//int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int result = number1 % number2;
+
+// if (result == 0)
+// {
+//    Console.WriteLine("Number1 can be devided by Number2 without remainder");
+// }
+// else
+// {
+//     Console.WriteLine($"Number1 can't be devided by Number2 without remainder, Remainder = {result}");
+// }
+
+Console.WriteLine("Type first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = Multiple (num1, num2);
+
+int Multiple (int num1, int num2)
+{
+   return num1%num2;
+}
+
+if (result == 0)
+{
+    Console.WriteLine("Number1 can be devided by Number2 without remainder");
+}
+else
+{
+     Console.WriteLine($"Number1 can't be devided by Number2 without remainder, Remainder = {result}");
+}
