6291abac8ff156d6b37043a44786502ef538515c
Sergey <litf@yandex.ru>
2023-03-26 19:24:57 +0300

    Task17

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Type dot coordinates");
+Console.Write("x:");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+Console.Write("y:");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+int quarter = Quarter(xCoordinate, yCoordinate);
+string result = quarter > 0 
+            ? $"Typed coordinates related to quarter - {quarter}"
+            : "Coordinates incorrect";
+
+Console.WriteLine(result);
+
+int Quarter(int x, int y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
